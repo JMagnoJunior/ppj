bc3967318f8d7bb58fb5fbd03dd0148f1d7b75ec
Magno <is.magnojr@gmail.com>
2015-05-12 22:31:54 -0300

    melhoria na leitura do codigo

--- ppj/util.py
+++ ppj/util.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Modulo que contem classes e funções utilitarias"""
 from datetime import date
 
 def enum(**enums):
+    """Simula um enum Java
+    """
     return type('Enum', (), enums)
 
 def formata_data(data_nascimento):
-    result = data_nascimento 
-    if type(data_nascimento) is not date:
+    """formata data de nascimento para o tipo date
+    """
+    result = data_nascimento
+    if not isinstance(data_nascimento, date):
         data_split = data_nascimento.split('-')
-        result = date(int(data_split[0]),int(data_split[1]),int(data_split[2]) )
+        result = date(int(data_split[0]), int(data_split[1]), int(data_split[2]) )
         
     return result
